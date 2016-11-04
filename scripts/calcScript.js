3990f99adc910cc00bff2094352215d0fcdca0af
JHenderson219 <hendersonj219@gmail.com>
2016-11-03 17:53:31 -0700

    jQuery issue fixed, 7 works

--- scripts/calcScript.js
+++ scripts/calcScript.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
-	$("#btn7").on('click', function(){
-		$("#calcBox").html("7");
+	var clickNumber = 0;
+	var currentCalc = [];
+	//Make generic "clicked" functions for numebers and operators, then call them on click events with jQuery.
+	/*function numberClicked(num){
+		if (clickNumber===0){
+			$("#calcScreen").empty().append(num).addClass("text-right");
+			currentCalc.push(num);
+			clickNumber++;
+		} else if (clickNumber > 0){
+			$("#calcScreen").append(num);
+			currentCalc.push(num);
+			clickNumber++;
+		}
+	}*/
+
+	$("#btn7").on('click',function(){
+		if (clickNumber===0){
+			$("#calcScreen").empty().append(7).addClass("text-right");
+			currentCalc.push(7);
+			clickNumber++;
+		} else if (clickNumber > 0){
+			$("#calcScreen").append(7);
+			currentCalc.push(7);
+			clickNumber++;
+		}
 	});
+
 });
