c0b9e387c6a1c8a1fb63e50a1d0caff6a9dbbe99
JHenderson219 <hendersonj219@gmail.com>
2016-11-04 18:49:00 -0700

    Final 11/4 update

--- scripts/calcScript.js
+++ scripts/calcScript.js
@@ -2,23 +2,44 @@
 $(document).ready(function() {
 	var placeNumber = 0;
 	var currentCalc = [];
+	var consolidatedCalc = [];
 	var calcIsReset = true;
+	var operators = /[+-/*]/gi;
+	var decimal = /./gi;
 	function resetCalc(){
 		currentCalc = [];
+		consolidatedCalc =[];
 		calcIsReset = true;
 		$("#calcScreen").empty().append(0);
 	};
 	function numberClicked(num){
 	if (calcIsReset === true){
 		$("#calcScreen").empty().append(num).addClass("text-right");
-			currentCalc.push(num);
+			currentCalc.unshift(num);
 			calcIsReset = false;
 	} else if (calcIsReset === false){
 		$("#calcScreen").append(num);
-		currentCalc.push(num);
+		currentCalc.unshift(num);
 		}
 	}
-
+	function operatorClicked(op){
+		$("calcScreen").empty().append(0).addClass("text-right");
+		currentCalc.unshift(op);
+		calcIsReset = true;
+	}
+	function runCalc(){
+		var  consolidator = [];
+		for (var i=0; i<currentCalc.length; i++){
+			if (typeof currentCalc[i] === "number" && i===0){
+				consolidator.push(currentCalc[i]);
+			} else if (currentCalc[i]===operators && i===0){
+				consolidator.push(0);
+				//DO SOMETHING WITH OPERATOR (currentCalc[i]);
+			}else if (typeof currentCalc[i]=== "number" && i>0){
+				consolidator.push(currentCalc[i]);
+			}
+		}
+	}
 	//Click Events for buttons.
 	$("#btnClear").click(function(){
 		resetCalc();
@@ -33,6 +54,7 @@ $(document).ready(function() {
 		numberClicked(9);
 	});
 	/*$("#btnDivide").click(function(){  //DIVIDE IS HERE
+		operatorClicked("/");
 	});*/
 	$("#btn4").click(function(){
 		numberClicked(4);
@@ -44,6 +66,7 @@ $(document).ready(function() {
 		numberClicked(6);
 	});
 	/*$("#btnTimes").click(function(){  //TIMES IS HERE
+		operatorClicked("*");
 	});*/
 	$("#btn1").click(function(){
 		numberClicked(1);
@@ -55,14 +78,18 @@ $(document).ready(function() {
 		numberClicked(3);
 	});
 	/*$("#btnMinus").click(function(){  //MINUS IS HERE
+		operatorClicked("-");
 	});*/
 	$("#btn0").click(function(){
 		numberClicked(0);
 	})
-	/*$("#btnPeriod").click(function(){  //PERIOD IS HERE
+	/*$("#btnPeriod").click(function(){  //PERIOD/DECIMAL IS HERE
 	});*/
+	
 	/*$("#btnPlus").click(function(){  //PLUS IS HERE
+		operatorClicked("+");
 	});*/
 	/*$("#btnEquals").click(function(){  //EQUALS IS HERE
+		runCalc();
 	});*/
 });
