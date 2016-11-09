0c722f6d92c48f68432d2b12a1098fa21e46992f
JHenderson219 <hendersonj219@gmail.com>
2016-11-08 16:01:25 -0800

    Basic Calc function working - beginning debugging

--- scripts/calcScript.js
+++ scripts/calcScript.js
@@ -1,61 +1,83 @@
 
 $(document).ready(function() {
-	var placeNumber = 0;
-	var currentNum = "";
-	var currentCalc = [];
-	var consolidatedCalc = [];
+	var holdNum = 0;
+	var currentNum = 0;
+	var currentOperator;
+	var operationReady = false;
 	var calcIsReset = true;
-	var operators = /[+-/*]/gi;
-	var decimal = /./gi;
+	var operatorReady = true;
+	var result = 0;
+
 	function resetCalc(){
-		currentCalc = [];
-		consolidatedCalc =[];
-		currentNum = "";
+		currentNum = 0;
+		holdNum = 0;
 		calcIsReset = true;
+		operationReady = false;
+		operatorReady = true;
+		currentOperator = undefined;
+		result = 0; 
 		$("#calcScreen").empty().append(0);
+		console.log("Reset Complete!")
+	}
+
+	function resetAfterEquals(){
+		calcIsReset = true;
+		operationReady = false;
+		operatorReady = true;
+		currentOperator = undefined;
+		console.log("Reset after Equals complete!");
+	}
+	function decimalClicked(dec){
+		console.log("decimal clicked!");
 	}
 	function numberClicked(num){
+	console.log("number clicked!")
 	if (calcIsReset === true){
+		console.log("Is calc reset? "+ calcIsReset);
 		$("#calcScreen").empty().append(num).addClass("text-right");
-			currentNum = num.toString();
-			calcIsReset = false;
-			console.log(currentNum);
+		holdNum = currentNum;
+		currentNum = num.toString();
+		calcIsReset = false;
+		console.log("holdNum is "+holdNum);
+		console.log("current number is "+currentNum);
 	} else if (calcIsReset === false){
+		console.log("Is calc reset? "+ calcIsReset);
 		$("#calcScreen").append(num);
-		currentNum.concat(num.toString);
-		console.log(currentNum);
+		currentNum = currentNum.concat(num.toString());
+		console.log("holdNum is "+holdNum);
+		console.log("current number is "+currentNum);
 		}
 	}
+	
 	function operatorClicked(op){
-		$("calcScreen").empty().append(0).addClass("text-right");
-		currentCalc = op;
-		calcIsReset = true;
-	}
-	function runCalc(){
-		var consolidator = [];
-		for (var i=0; i<currentCalc.length; i++){
+		console.log("Operator "+op+" clicked!");
+		console.log ("Is operationReady? "+operationReady);
+		console.log ("Is operatorReady? "+operatorReady);
+		if (operatorReady === true && operationReady === false){
+			holdNum = currentNum;
+			currentOperator = op;
+			$("#calcScreen").empty().append(0).addClass("text-right");
+			calcIsReset = true;
+			operatorReady = false;
+			operationReady = true;
+			console.log("holdNum is "+holdNum);
+			console.log("current number is "+currentNum);
+			console.log("currentOperator is " + op);
+		} else if (operatorReady === false && operationReady === true){
+			console.log("Chaining operation..."+holdNum.toString()+currentOperator+currentNum.toString());
+			runCalc(holdNum,currentOperator,currentNum);
+			$("#calcScreen").empty().append(currentNum).addClass("text-right");
+		} else if (operatorReady === false){
+			currentOperator = op;
 		}
 	}
-	/*function runCalc(){ //OLD DUMB runCalc!!!
-		var  consolidator = [];
-		for (var i=0; i<currentCalc.length; i++){
-			if (typeof currentCalc[i] === "number" && i===0){
-				consolidator.push(currentCalc[i]);
-				console.log("Is number and zero! Pushed "+currentCalc[i]+"!")
-			} else if (currentCalc[i]===operators && i===0){
-				consolidator.push(0);
+	
+	function runCalc(num1,operand,num2){ //MAKE SURE TO RESET ALL WHEN CALC ORDERED
+		var calcResult = eval(num1+operand+num2);
+		currentNum = calcResult;
+		return calcResult;
+	}
 
-				console.log("Is Operator and zero! Pushed "+0+"!")
-				//DO SOMETHING WITH OPERATOR (currentCalc[i]);
-			}else if (typeof currentCalc[i]=== "number" && i>0){
-				consolidator.push(currentCalc[i]);
-				console.log ("Is number and > 0! Pushed "+currentCalc[i]+"!");
-			}else if (currentCalc[i]===operators && i>0){
-
-				console.log("Is operator and > 0! Pushed "+currentCalc[i]+"!");
-			}
-		}
-	}*/
 	//Click Events for buttons.
 	$("#btnClear").click(function(){
 		resetCalc();
@@ -69,9 +91,9 @@ $(document).ready(function() {
 	$("#btn9").click(function(){
 		numberClicked(9);
 	});
-	/*$("#btnDivide").click(function(){  //DIVIDE IS HERE
+	$("#btnDivide").click(function(){  //DIVIDE IS HERE
 		operatorClicked("/");
-	});*/
+	});
 	$("#btn4").click(function(){
 		numberClicked(4);
 	});
@@ -81,9 +103,9 @@ $(document).ready(function() {
 	$("#btn6").click(function(){
 		numberClicked(6);
 	});
-	/*$("#btnTimes").click(function(){  //TIMES IS HERE
+	$("#btnTimes").click(function(){  //TIMES IS HERE
 		operatorClicked("*");
-	});*/
+	});
 	$("#btn1").click(function(){
 		numberClicked(1);
 	});
@@ -93,19 +115,33 @@ $(document).ready(function() {
 	$("#btn3").click(function(){
 		numberClicked(3);
 	});
-	/*$("#btnMinus").click(function(){  //MINUS IS HERE
+	$("#btnMinus").click(function(){  //MINUS IS HERE
 		operatorClicked("-");
-	});*/
-	$("#btn0").click(function(){
-		numberClicked(0);
 	});
-	/*$("#btnPeriod").click(function(){  //PERIOD/DECIMAL IS HERE
-	});*/
+	$("#btn0").click(function(){
+		if(calcIsReset===true){
+		} else if (calcIsReset===true){
+		numberClicked(0);	
+		}
+	});
+	$("#btnPeriod").click(function(){  //PERIOD/DECIMAL IS HERE
 	
-	/*$("#btnPlus").click(function(){  //PLUS IS HERE
+	});
+	
+	$("#btnPlus").click(function(){  //PLUS IS HERE
 		operatorClicked("+");
-	});*/
-	/*$("#btnEquals").click(function(){  //EQUALS IS HERE
-		runCalc();
-	});*/
+	});
+	$("#btnEquals").click(function(){  //EQUALS IS HERE
+		if (currentOperator != undefined){
+		console.log("Equals Executing...");
+		var result = runCalc(holdNum,currentOperator,currentNum);
+		$("#calcScreen").empty().append(result);
+		console.log("result of calculation is: "+result);
+		console.log("current num is now: "+currentNum);
+		resetAfterEquals();
+		} else {
+		alert("Please select an operator first! Calculator has been reset.")
+		resetCalc();
+		}
+	});
 });
